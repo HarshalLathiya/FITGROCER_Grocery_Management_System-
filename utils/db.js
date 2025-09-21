d641706920e5f3f4e06c5041f83aadfc460165d9
Mihir Lathiya <mihirlathiya2@gmail.com>
2025-09-21 10:18:49 +0530

    fix: db.js enhancement

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
   database: "fitgrocer",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0
 });
 
-module.exports = connection.promise();
+module.exports = pool.promise();
